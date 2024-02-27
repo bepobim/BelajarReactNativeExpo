9b7b1cb1d2ad52dc89168c5c91cc68ef9b368513
unknown <grawrtcuki@gmail.com>
2024-02-27 09:25:39 +0700

    ReactNativeJs

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [title, setTitle] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <ScrollView>
+        <Text>Test</Text>
+        <Text>{title}</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text
+            style={styles.buttonText}
+            onPress={() => setTitle("Hello Dunia")}
+          >
+            Submit
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    width: "100%",
+  },
+  button: {
+    width: "fit-content",
+    backgroundColor: "red",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    borderRadius: "5px",
+  },
+  buttonText: {
+    color: "white",
   },
 });
